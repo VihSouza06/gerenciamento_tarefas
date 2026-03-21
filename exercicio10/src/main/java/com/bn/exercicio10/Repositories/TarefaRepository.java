98cd1be48a3c5e48f1b7ad6009ec015636d52e8f
VihSouza06 <visouzasantos2006@gmail.com>
2026-03-21 16:41:15 -0300

    Criação do Repository

---
+++ exercicio10/src/main/java/com/bn/exercicio10/Repositories/TarefaRepository.java
@@ -0,0 +1,6 @@
+package com.bn.exercicio10.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TarefaRepository extends JpaRepository<TarefaRepository, Long> {
+}
