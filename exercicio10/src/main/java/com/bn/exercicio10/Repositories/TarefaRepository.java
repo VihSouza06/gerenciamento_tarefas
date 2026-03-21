606663b8fbd3ffaeb07ac97197e96887d29d0286
VihSouza06 <visouzasantos2006@gmail.com>
2026-03-21 16:50:29 -0300

    Criação do Service

--- exercicio10/src/main/java/com/bn/exercicio10/Repositories/TarefaRepository.java
+++ exercicio10/src/main/java/com/bn/exercicio10/Repositories/TarefaRepository.java
@@ -1,6 +1,7 @@
 package com.bn.exercicio10.Repositories;
 
+import com.bn.exercicio10.Models.TarefaModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TarefaRepository extends JpaRepository<TarefaRepository, Long> {
+public interface TarefaRepository extends JpaRepository<TarefaModel, Long> {
 }
---
+++ exercicio10/src/main/java/com/bn/exercicio10/Services/TarefaService.java
@@ -0,0 +1,37 @@
+package com.bn.exercicio10.Services;
+
+import com.bn.exercicio10.Models.TarefaModel;
+import com.bn.exercicio10.Repositories.TarefaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TarefaService {
+
+    @Autowired
+    private TarefaRepository tarefaRepository;
+
+    public List<TarefaModel> findAll(){
+        return tarefaRepository.findAll();
+    }
+
+    public TarefaModel criarTarefa(TarefaModel tarefaModel){
+        return tarefaRepository.save(tarefaModel);
+    }
+
+    public void deletarTarefa(Long id){
+        tarefaRepository.deleteById(id);
+    }
+
+    public TarefaModel buscarTarefaPorId(Long id){
+        return tarefaRepository.findById(id).get();
+    }
+
+    public TarefaModel atualizarTarefa(Long id, TarefaModel tarefaModel){
+        TarefaModel newTarefaModel = tarefaRepository.findById(id).get();
+        return tarefaRepository.save(tarefaModel);
+    }
+
+}
