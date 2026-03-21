a9fbc49d2969d657d9fce28bf1335e51be8711ca
VihSouza06 <visouzasantos2006@gmail.com>
2026-03-21 17:03:54 -0300

    Criação do Controller

---
+++ exercicio10/src/main/java/com/bn/exercicio10/Controllers/TarefaController.java
@@ -0,0 +1,52 @@
+package com.bn.exercicio10.Controllers;
+
+import com.bn.exercicio10.Models.TarefaModel;
+import com.bn.exercicio10.Services.TarefaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.ServletRequestBindingException;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/tarefas")
+public class TarefaController {
+
+    @Autowired
+    private TarefaService tarefaService;
+
+    @GetMapping
+    public ResponseEntity<List<TarefaModel>> findAll(){
+        List<TarefaModel> tarefa = tarefaService.findAll();
+        return ResponseEntity.ok().body(tarefa);
+    }
+
+    @GetMapping("/{id}")
+    public TarefaModel buscarTarefaPorId(@PathVariable Long id){
+        return tarefaService.buscarTarefaPorId(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<TarefaModel> criarTarefa(@RequestBody TarefaModel tarefaModel){
+        TarefaModel tarefaCriada = tarefaService.criarTarefa(tarefaModel);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri()
+                .path("/{id}").buildAndExpand(tarefaModel.getId()).toUri();
+        return ResponseEntity.created(uri).body(tarefaCriada);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deletarTarefa(@PathVariable Long id){
+        tarefaService.deletarTarefa(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<TarefaModel> atualizarTarefa
+            (@PathVariable Long id, @RequestBody TarefaModel tarefaModel){
+        TarefaModel tarefaAtualizada = tarefaService.atualizarTarefa(id, tarefaModel);
+        return ResponseEntity.ok(tarefaAtualizada);
+    }
+}
